7e40701cad07e308fcec5ae9d2c83c77e5e0e0e7
ShameimaruAya <wzszwj2014@gmail.com>
2020-02-26 13:44:43 +0800

    Revert "the dummy system is evil"
    This reverts commit c3fa58c5c93b27d904cf6be8dbde030fa8497880.

--- server/map.cpp
+++ server/map.cpp
@@ -4047,13 +4047,10 @@ if(false)
             
             unsigned char keyLoop[50];
             unsigned char valueLoop[512];
-            unsigned char valueTmp[512];
             
-            DB_Iterator dbIterator;
-            DB_Iterator_init( &playerDB, &dbIterator );
-            while( DB_Iterator_next( &dbIterator, keyLoop, valueLoop ) > 0 ) {
-                
-                Dummy* dummy = writeDummy()
+            DB_Iterator ecoIterator;
+            DB_Iterator_init( &playerDB, &ecoIterator );
+            while( DB_Iterator_next( &ecoIterator, keyLoop, valueLoop ) > 0 ) {
                 // int tmp = valueToInt( &( valueEco[0] ) );
                 // float money = static_cast<float>(tmp);
                 // memcpy(&tmp, &money, 4);
@@ -4603,34 +4600,8 @@ static void rememberDummy( FILE *inFile, int inX, int inY,
         }
     }
 
-static int readDummy(char marker, int parentID, int dummyIndex) {
-    ObjectRecord *parent = getObject( parentID );
-                
-    int dummyID = -1;
-    
-    if( parent != NULL ) {
-        
-        if( marker == 'u' && parent->numUses-1 > dummyIndex ) {
-            dummyID = parent->useDummyIDs[ dummyIndex ];
-            }
-        else if( marker == 'v' && 
-                 parent->numVariableDummyIDs > dummyIndex ) {
-            dummyID = parent->variableDummyIDs[ dummyIndex ];
-        } else
-            return parentID;
-    }
 
-    return dummyID;
-}
-
-struct Dummy
-{
-    char marker;
-    int parentID;
-    int dummyIndex;
-};
-
-static Dummy* writeDummy( FILE *inFile, char *email, 
+static void rememberDummy( FILE *inFile, char *email, 
                            ObjectRecord *inDummyO, 
                            int inSlot = -1, int inB = 0 ) {
     
@@ -4642,7 +4613,6 @@ static Dummy* writeDummy( FILE *inFile, char *email,
     int dummyIndex = -1;
 
     char marker = 'x';
-
 
     if( inDummyO->isUseDummy ) {
         marker = 'u';
@@ -4676,13 +4646,18 @@ static Dummy* writeDummy( FILE *inFile, char *email,
         }
     
     if( parent > 0 && dummyIndex >= 0 ) {
-        Dummy* dummy = new Dummy();
-        dummy->marker = marker;
-        dummy->parentID = parent;
-        dummy->dummyIndex = dummyIndex;
+        if( inSlot == -1 && inB == 0 ) {   
+            fprintf( inFile, "%s %c %d %d\n", 
+                     email, 
+                     marker, parent, dummyIndex );
+            }
+        else {
+            fprintf( inFile, "%s %c %d %d [%d %d]\n", 
+                     email, 
+                     marker, parent, dummyIndex, inSlot, inB );
+            }
+        }
     }
-    return NULL;
-}
 
 
 void freeMap( char inSkipCleanup ) {

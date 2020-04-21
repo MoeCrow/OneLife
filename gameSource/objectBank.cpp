d549bff68906688876c579710c46ba878fc30a93
ShameimaruAya <wzszwj2014@gmail.com>
2020-04-21 23:05:06 +0800

    Revert "FIX"
    This reverts commit f30d5e606b965770f6e0951bf45f5dde96f4d83b.

--- gameSource/objectBank.cpp
+++ gameSource/objectBank.cpp
@@ -597,6 +597,11 @@ static void setupWall( ObjectRecord *inR ) {
 static void setupTapout( ObjectRecord *inR ) {
     inR->isTapOutTrigger = false;
     
+    if( inR->isUseDummy || inR->isVariableDummy ) {
+        // only parent object counts tapouts
+        return;
+        }
+    
 
     char *triggerPos = strstr( inR->description, "+tapoutTrigger" );
                 

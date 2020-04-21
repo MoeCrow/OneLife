a55653820af7343b6d489b02fba1d93120bff1bc
ShameimaruAya <wzszwj2014@gmail.com>
2020-04-21 23:04:31 +0800

    Revert "修复"
    This reverts commit e983d4d9185ff66bb8913d5cb2c5788fb6b62094.

--- gameSource/objectBank.cpp
+++ gameSource/objectBank.cpp
@@ -601,7 +601,7 @@ static void setupTapout( ObjectRecord *inR ) {
 			int MainID = inR->useDummyParent;
 			if (MainID != 0) {
 				TapoutRecord r;
-				TapoutRecord r1 = *getTapoutRecord(MainID);
+				TapoutRecord r1 = *getTapoutRecord();
 
 				r.triggerID = inR->id;
 				r.gridSpacingX = r1.gridSpacingX;

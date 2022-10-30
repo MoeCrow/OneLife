ca68e98dcc2dc46a3aeddb6820a3d8e774857328
洛星熠 <325669792@qq.com>
2022-10-30 20:51:11 +0800

    213

--- server/server.cpp
+++ server/server.cpp
@@ -19137,8 +19137,8 @@ int main() {
             spawnIntervalSeconds = curSpawnTime;
             setMapObject(-593,-591,84191);
             setMapObject(-550,-591,84191);
-            setMapObject(-591 -557,84191);
-            setMapObject(-554 -555,84191);
+            // setMapObject(-591 -557,84191);
+            // setMapObject(-554 -555,84191);
 
         }
 
